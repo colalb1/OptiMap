c444876f3abe1d3784960c04197d7d67f380aa06
Colin <34983726+colalb1@users.noreply.github.com>
2025-09-01 13:47:49 -0500

    Basic hashmap tests.

---
+++ tests/test_hashmap.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+
+#include "optimap/hashmap.hpp"
+
+TEST(HashMapTest, InsertAndLookup) {
+    optimap::HashMap<int, std::string> map;
+
+    EXPECT_TRUE(map.insert(1, "one"));
+    EXPECT_TRUE(map.insert(2, "two"));
+
+    auto val1 = map.find(1);
+    ASSERT_TRUE(val1.has_value());
+    EXPECT_EQ(val1.value(), "one");
+
+    auto val2 = map.find(2);
+    ASSERT_TRUE(val2.has_value());
+    EXPECT_EQ(val2.value(), "two");
+
+    auto val3 = map.find(3);
+    EXPECT_FALSE(val3.has_value());
+}
+
+TEST(HashMapTest, OverwriteValue) {
+    optimap::HashMap<int, std::string> map;
+    EXPECT_TRUE(map.insert(1, "one"));
+    EXPECT_FALSE(map.insert(1, "uno"));  // Returns false if present
+
+    auto val = map.find(1);
+    ASSERT_TRUE(val.has_value());
+    EXPECT_EQ(val.value(), "one");
+}
+
+TEST(HashMapTest, RemoveKey) {
+    optimap::HashMap<int, std::string> map;
+    EXPECT_TRUE(map.insert(1, "one"));
+    EXPECT_TRUE(map.erase(1));
+
+    auto val = map.find(1);
+    EXPECT_FALSE(val.has_value());
+
+    EXPECT_FALSE(map.erase(1));  // already erased
+}
