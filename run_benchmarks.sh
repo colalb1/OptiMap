75146295b1eefeac3ae65d1059f40434e6eaff15
Colin <34983726+colalb1@users.noreply.github.com>
2025-09-07 11:18:10 -0500

    Clearing cache and adding iteration numbers.

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -13,8 +13,20 @@ mkdir -p benchmark_results
 for i in {1..14}
 do
     ./build/OptiMapBenchmarks --benchmark_out=benchmark_results/results_200000_$i.csv --benchmark_out_format=csv --benchmark_filter=200000
+    echo "Finished 200,000 iteration: " $i
+    sync
+    sudo purge
     ./build/OptiMapBenchmarks --benchmark_out=benchmark_results/results_2000000_$i.csv --benchmark_out_format=csv --benchmark_filter=2000000
+    echo "Finished 2,000,000 iteration: " $i
+    sync
+    sudo purge
+    ./build/OptiMapBenchmarks --benchmark_out=benchmark_results/results_20000000_$i.csv --benchmark_out_format=csv --benchmark_filter=20000000
+    echo "Finished 20,000,000 iteration: " $i
+    sync
+    sudo purge
 done
 
 # Generate the plots
 python3 plot_benchmarks.py benchmark_results/*.csv
+
+echo "Benchmark analysis complete. Plots are available in the 'plots' directory."
